0ca0de074742e6ed5b74cbdd9cad45777b92830b
AlexandrLystopadov <o.o.lystopadov@student.csn.khai.edu>
2024-07-04 20:24:19 +0300

    fix code

--- src/scripts.js
+++ src/scripts.js
@@ -2,10 +2,10 @@ function add(a, b) {
 	return a + b;
 }
 
-const result = add(2, 3);
+// const result = add(2, 3);
 
-const resultDiv = document.getElementById('result');
+// const resultDiv = document.getElementById('result');
 
-resultDiv.textContent = 'Result: ' + result;
+// resultDiv.textContent = 'Result: ' + result;
 
 module.exports = add;
